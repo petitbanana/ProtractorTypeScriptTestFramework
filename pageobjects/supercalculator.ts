3f829835083cb77fc48cc6b0fa5804062908f042
petitbanana <yuanming.hou@gmail.com>
2019-12-11 20:29:06 -0500

    first remote commit

--- pageobjects/supercalculator.ts
+++ pageobjects/supercalculator.ts
@@ -6,17 +6,17 @@ export class superCalculator {
     header = element(by.css("body > div > div > h3"));
     result = element(by.css("ng-binding"))
     addition(a,b: number){
-        element(by.id("first")).sendKeys(a);
-        element(by.id("operator")).element(by.cssContainingText("+")).click();
-        element(bv.id("second")).sendKeys(b);
-        element(by.id("gobutton")).click();
+        element(by.model("first")).sendKeys(a);
+        element(by.model("operator")).element(by.cssContainingText("+")).click();
+        element(bv.model("second")).sendKeys(b);
+        element(by.model("gobutton")).click();
     }
 
     subtraction(a,b:number){
-        element(by.id("first")).sendKeys(a);
-        element(by.id("operator")).element(by.cssContainingText("-")).click();
-        element(bv.id("second")).sendKeys(b);
-        element(by.id("gobutton")).click();
+        element(by.model("first")).sendKeys(a);
+        element(by.model("operator")).element(by.cssContainingText("-")).click();
+        element(bv.model("second")).sendKeys(b);
+        element(by.model("gobutton")).click();
     }
 
 
